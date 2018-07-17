7e605c52edad0756c8fd270ecdbf32ad8e265011
katesliang <kateliangg@gmail.com>
2018-07-17 21:08:08 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+  "My name is Kate"
+end
+get '/hometown' do
+  "My hometown is Plano, Texas."
+end
+get '/favorite-song' do
+  "My favorite song is Made in China by Higher Brothers"
+end
 end

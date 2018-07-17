557a73b8a282d323f02d112a3585945ac32d0def
lauratan878 <lauratan878@gmail.com>
2018-07-17 21:13:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+  "Hello, World!"
 end
+  get '/name' do
+    "My name is Laura"
+  end
+  get '/hometown' do
+    "My hometown is Plano"
+  end
+  get '/favorite-song' do
+    "My favorite song is You Belong With Me"
+end
+end 
+  
